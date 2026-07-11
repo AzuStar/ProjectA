28127aa4e0134d72f116f1244aaca1c6611db41e
Teljen-tech <180743084+Teljen-tech@users.noreply.github.com>
2026-07-11 10:40:37 +0100

    A scene you can run around in.

---
+++ Scripts/DroneGame/PlayerController.cs
@@ -0,0 +1,41 @@
+using Godot;
+
+public partial class PlayerController : CharacterBody3D
+{
+	private const float Speed = 5.0f;
+	private const float JumpVelocity = 4.5f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		Vector2 inputDir = Input.GetVector("game_left", "game_right", "game_forward", "game_back");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
