ba1ba08e35bbf30c178f9cdad091b087901ebebf
Teljen-tech <180743084+Teljen-tech@users.noreply.github.com>
2026-07-12 11:07:51 +0100

    One shot support.
    You could use this for a throwable switch that stays down.

--- Scripts/Interaction/TriggerVolume.cs
+++ Scripts/Interaction/TriggerVolume.cs
@@ -6,14 +6,25 @@ namespace ProjectA.Game.Interaction;
 
 public partial class TriggerVolume : Area3D
 {
+    /// <summary>
+    /// The triggerable this trigger volume will activate and deactivate.
+    /// </summary>
     [Export] private Triggerable _triggerable;
+    
+    /// <summary>
+    /// If set, activate once on entry and then never again, and never deactivate on exit.
+    /// </summary>
+    [Export] private bool _oneShot;
 
     public override void _Ready()
     {
         Debug.Assert(_triggerable != null, $"Trigger volume {Name} has no target triggerable set!");
 
         BodyEntered += OnBodyEntered;
-        BodyExited += OnBodyExited;
+        if (!_oneShot)
+        {
+            BodyExited += OnBodyExited;
+        }
     }
 
     private void OnBodyEntered(Node3D body)
@@ -24,6 +35,12 @@ public partial class TriggerVolume : Area3D
         }
 
         _triggerable.Activate();
+
+        if (_oneShot)
+        {
+            // We're done, goodbye!
+            BodyEntered -= OnBodyEntered;
+        }
     }
 
     private void OnBodyExited(Node3D body)
